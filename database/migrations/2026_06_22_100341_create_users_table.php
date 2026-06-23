a1f7e88024605e563c893215737a8ba9449180a8
Tey - nullthefirst <usheninte234@gmail.com>
2026-06-23 19:27:31 +0100

    Updated create users table migration

--- database/migrations/2026_06_22_100341_create_users_table.php
+++ database/migrations/2026_06_22_100341_create_users_table.php
@@ -10,13 +10,15 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('username')->unique();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
