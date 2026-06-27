f131481b5082b0e0bb91cd08685fe5661fd28e1d
Tey - nullthefirst <usheninte234@gmail.com>
2026-06-27 10:39:35 +0100

    Updated service page features logic check

--- resources/views/public/services/page.blade.php
+++ resources/views/public/services/page.blade.php
@@ -64,7 +64,7 @@
 
             <hr class="border-white/10">
 
-            @if(!empty($service->solutions))
+            @if(!empty($service->features))
             <div class="space-y-6">
                 <div>
                     <h3 class="text-xs tracking-widest font-bold text-opes-cyan uppercase font-mono">Core Competencies</h3>
