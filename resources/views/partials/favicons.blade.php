9ca666cfb7552f6f1eefccb69646346e8768d172
Tey - nullthefirst <usheninte234@gmail.com>
2026-06-29 13:54:19 +0100

    Added favicons

---
+++ resources/views/partials/favicons.blade.php
@@ -0,0 +1,16 @@
+<!-- Favicons -->
+<link
+  rel="icon"
+  type="image/png"
+  href="{{ Vite::asset('resources/favicons/favicon-96x96.png') }}"
+  sizes="96x96"
+/>
+<link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/favicons/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ Vite::asset('resources/favicons/favicon.ico') }}" />
+<link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href="{{ Vite::asset('resources/favicons/apple-touch-icon.png') }}"
+/>
+<meta name="apple-mobile-web-app-title" content="OPES" />
+<link rel="manifest" href="{{ Vite::asset('resources/favicons/site.webmanifest') }}" />
