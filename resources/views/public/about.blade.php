f1d301bdb55aeec7c13592f486ad98ab7e889e9f
Tey - nullthefirst <usheninte234@gmail.com>
2026-07-06 11:31:05 +0100

    Updated background image mobile responsiveness

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -6,7 +6,7 @@
     <section class="py-16">
         <div class="max-w-7xl mx-auto px-6">
             <!-- About Banner Section with Background Image -->
-            <section class="relative bg-cover bg-center bg-no-repeat py-20 md:py-32 mb-20 h-20" style="background-image: url('{{ Vite::asset("resources/images/banners/opes-about.png") }}');">
+            <section class="relative bg-cover bg-center bg-no-repeat py-20 md:py-32 mb-20 md:h-20" style="background-image: url('{{ Vite::asset("resources/images/banners/opes-about.png") }}');">
                 <!-- Overlay to keep text perfectly readable -->
                 <div class="absolute inset-0 bg-black/40"></div>
 
